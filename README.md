35bcd7692a123ae0b782766df05928d7aace2001
Jean Julien <reachme.jeanjulien@gmail.com>
2023-05-17 07:59:26 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Automation practice with Selenium
+
+## Project Description 
+
+This project contains a Selenium automation script for a purchase journey. 
+
+The project is quite simple, and so we tried to write is with simplicity using
+Python and Selenium. 
+
+## Prerequisites
+
+python 3.11
+Chromedriver corresponding to your chrome browser.
+Kindly pay attention to version compatibility between your driver and your browser. 
+Selenium 4.9.1
+
+
+## Setup 
+
+1. Clone the repo
+2. Navigate to the cloned repo and run "brew install pipenv" to install pipenv
+3. Run "pipenv install --dev" to install the dependencies
+
+
+## Execute the code
+
+Run: "pytest tests/test_purchase_journey.py
---
+++ tests/test_purchase_journey.py
@@ -0,0 +1,181 @@
+"""Automation practice code question 1"""
+
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+class Purchase_Journey():
+
+    def test_purchase_journey(driver):
+
+        driver = webdriver.Chrome()
+
+        try:
+            wait = WebDriverWait(driver, 10)
+            driver.get('http://automationpractice.pl/index.php')
+
+            driver.maximize_window()
+
+            # Click Sign in
+            sign_in_button = wait.until(EC.presence_of_element_located(
+                (By.CLASS_NAME, 'login')
+            ))
+            sign_in_button.click()
+
+            # Enter email
+            username = wait.until(EC.presence_of_element_located(
+                (By.ID, 'email')
+            ))
+            username.send_keys('testautomationmfs@gmail.com')
+
+            time.sleep(5)
+
+            # Enter password
+            password = driver.find_element(
+                By.CSS_SELECTOR, '[data-validate="isPasswd"]')
+            password.send_keys('TestAutomation@123')
+
+            time.sleep(8)
+
+            # Click Sign In button
+            driver.find_element(By.ID, 'SubmitLogin').click()
+
+            time.sleep(8)
+
+            # Locate Home button after login in to the platform
+            home_auth_link = wait.until(EC.presence_of_element_located(
+                (By.LINK_TEXT, 'Home')
+            ))
+
+            # Scroll down to Home button
+            driver.execute_script(
+                "window.scrollTo(0, arguments[0].getBoundingClientRect().top - 100);", home_auth_link)
+
+            time.sleep(8)
+
+            # Click Home button to go back to the landing page
+            home_auth_link.click()
+
+            time.sleep(8)
+
+            # Locate popular category
+            popular_category = wait.until(EC.presence_of_element_located(
+                (By.ID, 'homefeatured')
+            ))
+
+            # Scroll to popular category
+            driver.execute_script(
+                "window.scrollTo(0, arguments[0].getBoundingClientRect().top - 100);", popular_category)
+
+            time.sleep(10)
+
+            # Locate women category
+            women = wait.until(EC.presence_of_element_located(
+                (By.LINK_TEXT, 'WOMEN')
+            ))
+
+            # Mouse hover on women category
+            hover_dresses_category = ActionChains(driver)
+            hover_dresses_category.move_to_element(women)
+            hover_dresses_category.perform()
+
+            # Locate and click on evening dresses
+            evening_dresses = wait.until(EC.presence_of_element_located(
+                (By.LINK_TEXT, 'Evening Dresses')
+            ))
+            evening_dresses.click()
+
+            time.sleep(8)
+            # Filter by M size
+            m_size_checkbox = wait.until(EC.presence_of_element_located(
+                (By.ID, 'layered_id_attribute_group_2')
+            ))
+            m_size_checkbox.click()
+
+            time.sleep(8)
+
+            # Choose color pink
+            pink_color = driver.find_element(
+                By.CSS_SELECTOR, '[data-rel="24_3"]')
+            pink_color.click()
+
+            time.sleep(9)
+            # Locate price range slider
+            price_range_slider = driver.find_element(
+                By.ID, "layered_price_slider")
+
+            driver.execute_script(
+                "window.scrollTo(0, arguments[0].getBoundingClientRect().top - 100);", price_range_slider)
+
+            time.sleep(8)
+            # Filter by price range
+            drag_slider = ActionChains(driver)
+            drag_slider.drag_and_drop_by_offset(
+                price_range_slider, 65, 0).perform()
+            time.sleep(4)
+            drag_slider.release().perform()
+
+            time.sleep(10)
+            # Locate printed dress
+            printed_dress = wait.until(EC.presence_of_element_located(
+                (By.LINK_TEXT, 'Printed Dress')
+
+            ))
+            # Mouse over printed dress
+            hover_printed_dress = ActionChains(driver)
+            hover_printed_dress.move_to_element(printed_dress)
+            hover_printed_dress.perform()
+
+            time.sleep(10)
+            # Locate More button and click on it
+            view_button = wait.until(EC.presence_of_element_located(
+
+                (By.XPATH,
+                 '//*[@id="center_column"]/ul/li/div/div[2]/div[2]/a')
+
+            ))
+            view_button.click()
+
+            time.sleep(10)
+
+            # Locate quantity label
+
+            quantity_label = wait.until(EC.presence_of_element_located(
+                (By.XPATH, '//*[@id="quantity_wanted_p"]/label')
+            ))
+
+            # Let's see if quantity label is displayed
+
+            is_quantity_label_displayed = quantity_label.is_displayed()
+            is_product_available = is_quantity_label_displayed
+
+            print("Product quantity label is displayed: ",
+                  is_quantity_label_displayed)
+
+            # CHECK PRODUCT AVAILABILITY STATUS
+            # Locate the product availability span
+            products_availability_value = driver.find_element(
+                By.ID, 'availability_value')
+
+            availability_text = products_availability_value.text
+            print(availability_text)
+
+            is_product_availability_displayed = products_availability_value.is_displayed()
+            print(
+                "Product availability is displayed: ", is_product_availability_displayed)
+
+            if availability_text == "This product is no longer in stock" or not is_product_available:
+                print("The product seems to be no more in stock. "
+                      "We can't pursue the purchase journey.")
+            else:
+                print("Let's continue")
+
+            time.sleep(10)
+
+        finally:
+
+            driver.quit()
